089009814aeac7e603d4b5e173f3173a0931c3f5
dchakov <dchakov@yahoo.com>
2016-02-04 16:31:27 +0200

    create architecture

---
+++ Real Estates Application/RealEstates.Data/IRealEstatesDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace RealEstates.Data
+{
+    using RealEstates.Model;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    public interface IRealEstatesDbContext
+    {
+        IDbSet<User> Users { get; set; }
+
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+
+        int SaveChanges();
+
+        void Dispose();
+    }
+}
---
+++ Real Estates Application/RealEstates.Data/RealEstatesDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace RealEstates.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using RealEstates.Model;
+
+    public class RealEstatesDbContext : IdentityDbContext<User>
+    {
+        public RealEstatesDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static RealEstatesDbContext Create()
+        {
+            return new RealEstatesDbContext();
+        }
+    }
+}
